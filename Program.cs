12b24fc3ab126f29f8aa52562cd8428ace5af0b2
islom1994 <islommuxammad1994@gmail.com>
2023-03-04 16:28:53 +0500

    dz

--- Program.cs
+++ Program.cs
@@ -1,27 +1,41 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа
-// (A и B) и возводит число A в натуральную степень B.
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
+﻿//Задача 34: Задайте массив заполненный случайными положительными
+// трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
+
+
 
 // using System;
-
-// namespace Dz25
+// class Program
 // {
-//   class Program
+//   static void Main(string[]args)
 //   {
-//     static void Main(string[] args)
-//     {
-//       Console.Write("Введите чеслo  ");
-//       int numbers = int.Parse(Console.ReadLine());
-//       int number = int.Parse(Console.ReadLine());
-//       var a = numbers*numbers*numbers*numbers;
-//       var b = number*number*number*number*number;
-//       Console.WriteLine(a);
+//     int number = int.Parse(Console.ReadLine());
+//     int[] arr = new int[12];             
+//     int k = 0, sum = 0;             
+//     Random rnd = new Random();               
+//     Console.WriteLine("Исходный массив");             
+//     for (int i = 0; i < 12; i++)             
+//     {                 
+//       arr[i] = rnd.Next(-50, 50);                 
+//       Console.Write("{0} ", arr[i]);             
+//     }                          
+//     Console.WriteLine("Отсортированный массив ");             
+//     Array.Sort(arr);               
+//     foreach(int element in arr)             
+//     Console.Write("{0} ", element);                 
+//     if (element % 2 == 0)                 
+//     {                    
+//       k++;                     
+//       sum += element;       
+//       Console.WriteLine("Количество четных элементов {0}, сумма четных элементов {1}", k, sum);              
+//     }             
+//   }                     
+//  }                               
+// }          
+  
 
-//     }
-
-//   }
-// }
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
 
 
 
@@ -29,70 +43,40 @@
 
 
 
+// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
 
 
-
-// Задача 27: Напишите программу, которая принимает вход на число и 
-// выдает сумму, включающую сумму.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
- 
 // using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using System.Collections.ObjectModel;
-// using System.Linq;
-// using System.Text;
-// using System.Threading;
-//  namespace Lesson
-//  {
-//    class Program
-//    {
-    
-//      static void Main(string[] args)
-//      {
+// class Program
+// {
+//   static void Main(string[]args)
+//   {
+//        Console.WriteLine("Введите чесло ..");
+//        int num = int.Parse(Console.ReadLine()!);
+//        if(num <= 99)
+//        {
+//          Console.WriteLine("Третьей цифры нет ");
       
-//       Console.WriteLine("Введите сумму");
-//       int number = int.Parse(Console.ReadLine());
-//       int a;
-//       int b = number%10+number/10%10+number/100;
-//       Console.WriteLine(b);
-//    }
-//  }
-//  }
+//        }
+//        if(num >= 100 && num <= 999)
+//        {
+//          int num2 = num % 10;
+//          Console.WriteLine("num2");
+//        }
+//        if(num >= 1000 && num < 10000)
+//        {
+//          int num2 = num %100/10;
+//          Console.WriteLine(num2);
+//        }
+//        if(num >= 10000 && num <100000)
+//        {
+//          int num2 = num %1000 / 100;
+//          Console.WriteLine(num2);
 
-
-// Задача 29: Напишите программу, которая задает массив
-// из 8 элементов и выводит их на экран.
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-// 6, 1, 33 -> [6, 1, 33] 
-
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Globalization;
-// using System.Linq;
-//  namespace Lesson
-//  {
-//    class Program
-//    {
-    
-//      static void Main(string[] args)
-//      {
-//       Console.Write("Введите количество елементов массива:\t");
-//       int elementsCount = int.Parse(Console.ReadLine());
-//       int[]myArray = new int[elementsCount];
-//       for (int i = 0; i < myArray.Length; i++)
-//       {
-//         Console.Write($"\nВведите елементов массива под индексом {i}:\t");
-//         myArray[i] = int.Parse(Console.ReadLine());
-//       }
-//       Console.WriteLine("\nВывод массива:");
-//       for (int i = 0; i< myArray.Length; i++)
-//       {
-//         Console.WriteLine(myArray[i]);
-//       }
-//      }
-//    }
-// }
+//        }
+//      }              
+                 
+//    }     
+//  }   
+        
